9142aab4cd74750f935620e8c72180d46648ae48
Sam15-code <ibtissam.dxb@gmail.com>
2023-05-18 12:31:42 -0700

    creating server

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+var bodyParser = require("body-parser");
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
+app.use(bodyParser.json());
+const dotenv = require("dotenv");
+
+dotenv.config();
+// require("./config/mongodb_connection");
+app.get("/", (req, res) => {
+  res.send({ message: "server working" });
+});
+
+const PORT = process.env.PORT || 4433;
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(PORT, console.log("Server started ..."));
