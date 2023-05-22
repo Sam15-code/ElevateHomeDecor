4881fac7459c4f4fe2ec11112fb3558f9ba96572
eelnimeiry <Elsadekelnimeriy@gmail.com>
2023-05-22 10:58:49 -0700

    creating redux setup browser routes and providers

--- client/src/index.js
+++ client/src/index.js
@@ -15,15 +15,25 @@ import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <OpenCartContextProvider>
-        <SnackBarContextProvider>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </SnackBarContextProvider>
-      </OpenCartContextProvider>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <OpenCartContextProvider>
+          <SnackBarContextProvider>
+            <PayPalScriptProvider
+              options={{
+                "client-id": "test",
+                components: "buttons",
+                currency: "USD",
+              }}
+            >
+              <BrowserRouter>
+                <App />
+              </BrowserRouter>
+            </PayPalScriptProvider>
+          </SnackBarContextProvider>
+        </OpenCartContextProvider>
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
